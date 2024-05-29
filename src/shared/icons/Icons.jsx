af8e9e3646d456d52368c85a26074c43e6349af9
Maria Joaquina <84864759+MariJoaqui@users.noreply.github.com>
2024-05-29 15:17:33 -0400

    Falta agregar hover y tamaño a los íconos.

--- src/shared/icons/Icons.jsx
+++ src/shared/icons/Icons.jsx
@@ -2,9 +2,10 @@ import React from "react";
 
 function Icons({ icon, href }) {
 
+  // Arreglar los íconos
   return (
     <a href={href}>
-      <i className={`fa-brands fa-${icon} white-text`} ></i>
+      <i className={`fa-brands fa-${icon} fa-lg white-text icon`} ></i>
     </a>
   );
 }
