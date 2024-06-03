27b477e1ea99e2d7cccd7a22bcbeae253b0115ad
Maria Joaquina <84864759+MariJoaqui@users.noreply.github.com>
2024-06-03 15:24:10 -0400

    Optimización de código. Se agregó un nuevo archivo para los datos.

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -10,40 +10,10 @@ import Icons from "../shared/icons/Icons";
 import Cards from "../shared/cards/Cards";
 import Opinions from "../shared/opinions/Opinions";
 
+// Data
+import { icons, cards, opinions } from "../shared/data/data";
+
 function Home(props) {
-
-    { /* Icons */ }
-    const icons = [
-        { src: require("../files/images/i-whatsapp.png"), href: "#" },
-        { src: require("../files/images/i-instagram.png"), href: "https://www.instagram.com/businesscenterrd?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" },
-        { src: require("../files/images/i-youtube.png"), href: "#" }
-    ];
-
-    { /* Cards */ }
-    const cards = [
-        { title: "Secretaría remota", description: "Asistencia administrativa profesional sin necesidad de contratar personal a tiempo completo.", src: require("../files/images/servicios-1.jpg") },
-        { title: "CRM Gestión de clientes", description: "Optimiza la relación con tus clientes mediante sistemas CRM personalizados.", src: require("../files/images/servicios-2.jpg") },
-        { title: "Mensajería", description: " Servicio rápido y confiable para la entrega de documentos y paquetes.", src: require("../files/images/servicios-3.jpg") },
-        { title: "Agenda de citas inteligente", description: "Programa y gestiona tus citas con recordatorios automáticos y sincronización en tiempo real.", src: require("../files/images/servicios-1.jpg") },
-        { title: "Salón de conferencias", description: "Modernas salas equipadas con tecnología avanzada para reuniones y eventos.", src: require("../files/images/servicios-2.jpg") },
-        { title: "Grabación de Spot publicitario", description: "Creamos spots publicitarios impactantes y de alta calidad.", src: require("../files/images/servicios-3.jpg") },
-        { title: "Creación de Videos publicitarios", description: "Producción de videos publicitarios que destacan tu marca.", src: require("../files/images/servicios-1.jpg") },
-        { title: "Gestión de redes sociales", description: "Estrategias personalizadas para mejorar tu presencia en redes sociales.", src: require("../files/images/servicios-2.jpg") },
-        { title: "Diseño de Logotipo", description: "Logotipos únicos que reflejan la identidad de tu marca.", src: require("../files/images/servicios-3.jpg") },
-        { title: "Desarrollo de Página web", description: "Páginas web atractivas y funcionales para una excelente experiencia de usuario.", src: require("../files/images/servicios-1.jpg") },
-        { title: "Geofencing y marketing de proximidad", description: "Estrategias para atraer clientes en ubicaciones específicas.", src: require("../files/images/servicios-2.jpg") },
-        { title: "Presencia en distintas plataformas", description: "Establece y mantén una fuerte presencia en diversas plataformas digitales.", src: require("../files/images/servicios-3.jpg") }
-    ];
-
-    { /* Opinions */ }
-    const opinions = [
-        { title: "Inspire International", description: "Business Center representa una solida plataforma de apoyo para quienes desean incursionar con éxito en el emergente mercado Dominicano. Excelente atención e incomparable servicio." },
-        { title: "ITCS Americas S.A.", description: "Nos complacemos en recomendar ampliamente a la empresa Business Center. Cada profesional que la conforma ha sido de gran apoyo para nuestra estrategia y desarrollo comercial." },
-        { title: "Latin Center Division", description: "Nuestra experiencia utilizando los servicios de Business Center fue buena. Personas amables y serviciales que cumplían con lo solicitado, un salón de reuniones amplio y confortable, acompañado de un excelente servicio." },
-        { title: "Globa Terra", description: "Nos complacemos en calificar favorablemente y recomendar el servicio de Oficinas Virtuales, y periféricos, que brinda la empresa Business Center con sus instalaciones en Santo Domingo, República Dominicana." },
-    ];
-
-    { /* Slider */ }
     const [currentSlide, setCurrentSlide] = useState(0);
     const [loaded, setLoaded] = useState(false);
     const [sliderRef, instanceRef] = useKeenSlider({
